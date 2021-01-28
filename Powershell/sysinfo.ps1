d533bbfb4303ab289988b61fbcacafdee04b48f6
Patrick Mileham <milehapk@mail.uc.edu>
2021-01-28 13:34:38 -0500

    sysinfo powershell script

--- Powershell/sysinfo.ps1
+++ Powershell/sysinfo.ps1
@@ -5,3 +5,13 @@
 write-host(getip)
 $ip = getip
 write-host("This machines IP Address is: $ip")
+
+$hostname = hostname
+
+$date = Get-Date
+
+$Body = "This machines IP is $ip. User is $env:username. Hostname is $hostname. Today's date is $date"
+
+write-host($Body)
+
+Send-MailMessage -To "milehapk@mail.uc.edu" -From "natthekiddd@gmail.com" -Subject "IT3038C windows Sysinfo" -Body $Body -SmtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-credential)
